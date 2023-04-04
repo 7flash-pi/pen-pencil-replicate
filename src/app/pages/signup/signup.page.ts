3cb9c825a49c3e73a772d691e2e195d7c359b911
ashish <rashish0809.ar@gmail.com>
2023-04-04 18:23:26 +0530

    signup form

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { GlobalService } from 'src/app/provider/global-services/global.service';
 
@@ -9,13 +10,24 @@ import { GlobalService } from 'src/app/provider/global-services/global.service';
 })
 export class SignupPage implements OnInit {
 
+  signUpForm:FormGroup;
   phoneRegex:boolean=false;
+
   mobileNumber:number;
+  @ViewChild('btnNext',{
+    static:true
+  }) btn:ElementRef;
 
   constructor(private router:Router,
-    private globalService:GlobalService) { }
+    private globalService:GlobalService,
+    private fb:FormBuilder) { }
 
   ngOnInit() {
+
+    this.signUpForm=this.fb.group({
+      mobile:[this.mobileNumber,[Validators .required,Validators.maxLength(10),Validators.minLength(10),Validators.pattern(/[6789]{1}[0-9]{9}/)
+      ]]
+    })
   }
   gotoLogin(){
     this.router.navigate(['login']);
@@ -31,6 +43,7 @@ export class SignupPage implements OnInit {
         }
         else{
           this.globalService.showToast('Please enter a valid PhoneNumber',2000,'bottom');
+          this.signUpForm.reset();
 
         }
       }
