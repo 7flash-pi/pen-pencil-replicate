d1c7e072b245827fd04799ae52ee56151d25991c
ashish <rashish0809.ar@gmail.com>
2023-04-04 14:16:13 +0530

    migration

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobalService } from 'src/app/provider/global-services/global.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  phoneRegex:boolean=false;
+  mobileNumber:number;
+
+  constructor(private router:Router,
+    private globalService:GlobalService) { }
 
   ngOnInit() {
+  }
+  gotoLogin(){
+    this.router.navigate(['login']);
+  }
+
+  checkphoneNumber(input){
+    if(input.target && input.target.value ){
+        this.phoneRegex = !!(input.target.value.match(/[6789]{1}[0-9]{9}/));
+      }
+      if(input.target && input.target.value.length === 10 ){
+        if(this.phoneRegex){
+          input.target.blur();
+        }
+        else{
+          this.globalService.showToast('Please enter a valid PhoneNumber',2000,'bottom');
+
+        }
+      }
   }
 
 }
